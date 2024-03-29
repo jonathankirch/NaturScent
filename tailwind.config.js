6e05f4d200386ce6b2dc39b159b0c132daa8195c
Jonathan Kirch <jonathankirch101@gmail.com>
2024-03-29 15:22:32 -0300

    lançando projeto NaturScent

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+	theme: {
+		extend: {
+			colors: {
+				background: '#FFFCFC',
+				green: '#6D775C',
+				'green-light': '#E1E5D6',
+				'green-light2': '#F8F0ED',
+			},
+		},
+	},
+};
